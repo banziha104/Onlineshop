f044425aa7a48241123e63032fcc24e9f56e06fb
iyeongjun <they6687@naver.com>
2017-10-13 01:07:29 +0900

    Add Model

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-
+from django.conf import settings              # 미디어 url 불러오기
+from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^',include('shop.urls',namespace='shop')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -11,4 +11,4 @@ def product_list(request, category_slug=None):
         category = get_object_or_404(Category, slug = category_slug)
         products = products.filter(category=category)
 
-    return render(request,'shop/product/list.html',{'category':category,'categories':categories,'product':products})
+    return render(request,'shop/product/list.html',{'category':category,'categories':categories,'products':products})
