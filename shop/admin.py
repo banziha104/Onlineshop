227162d5ae03cb89badf0dcd7d8a18b281998f3b
iyeongjun <they6687@naver.com>
2017-10-12 21:53:38 +0900

    Add Model

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Category,Product
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)} # name을 기준으로 자동으로 생성해라
+admin.site.register(Category,CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','category','price','stock','available','created','updated']
+    list_filter =  ['available','created','updated','category'] # 우측생성 되는 필터
+    list_editable = ['price','stock','available']               # 수정으로 들어가지 않더라도 목록에서 수정가능
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Product,ProductAdmin)
--- shop/models.py
+++ shop/models.py
@@ -14,8 +14,28 @@ class Category(models.Model):
     def __str__(self):                  # 어떤 값을 출력할지
         return self.name
 
-    def get_absolute_url(self):         
+    def get_absolute_url(self):
         return reverse('shop:product_list_by_category',args=[self.slug])
 
 class Product(models.Model):
-    pass
+    category = models.ForeignKey(Category,related_name='products')
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.CharField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True)
+
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10,decimal_places= 2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created',)            # 최신순
+        index_together = (('id','slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absoulte_url(self):
+        return reverse('shop:product_detail',args=[self.id,self.slug])
