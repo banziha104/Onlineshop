9f841c7de3c9e86ccc84c7e703f36e72228435ef
iyeongjun <they6687@naver.com>
2017-10-17 21:00:38 +0900

    Add Cart

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^',include('shop.urls',namespace='shop')),
+    url(r'^cart/',include('cart.urls', namespace= 'cart')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
