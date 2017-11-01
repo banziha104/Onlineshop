3eacb0e714a911410d283e59c1f1026629f1f9ee
iyeongjun <they6687@naver.com>
2017-11-01 16:26:25 +0900

    Add Order

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
 # OnlineShop with Django
 
-
-```python
-import django.core
-
-def AsyncMyTask(self):
-    if(a):
-        return a
-    else :
-        return self;
-```
--- shop/views.py
+++ shop/views.py
@@ -5,7 +5,6 @@ from cart.forms import CartAddProductForm
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product,id=id,slug=slug,available=True)
-
     cart_product_form = CartAddProductForm()
     return render(request,'shop/product/detail.html',{'product':product,'cart_product_form':cart_product_form})
 
